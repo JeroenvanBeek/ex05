8e15a1faca9720c0cfcf53daeb6a04aed7b868ce
JeroenvanBeek <JJ.vanbeek@student.han.nl>
2020-08-30 20:42:35 +0200

    add a thread function

--- getsignal.c
+++ getsignal.c
@@ -4,8 +4,13 @@
 #include <string.h>  // memset
 #include <signal.h>  // sigaction, sigemptyset, struct sigaction, SIGINT
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/stat.h>       // mkfifo()
 #include <fcntl.h>          // open(), O_* constants
+#include <pthread.h>
+
+
+void *ThreadFunction(void *arg);
 
 volatile sig_atomic_t i = 0;
 void newHandler();//(int sig);
@@ -13,6 +18,10 @@ void newHandler();//(int sig);
 int main(void) {
   struct sigaction act, oldact;
   
+  pthread_t ThreadID_A;
+
+  const char ThreadArgA[] = "I am thread A.";
+
   // Define SHR:
   memset(&act, '\0', sizeof(act));  // Fill act with NULLs by default
   act.sa_handler = newHandler;      // Set the custom SHR
@@ -21,11 +30,19 @@ int main(void) {
   
   // Install SHR:
   sigaction(SIGINT, &act, &oldact);  // This cannot be SIGKILL or SIGSTOP
-  
-  // Count Ctrl-Cs pressed; continue upon Enter:
+
+
+  if(pthread_create(&ThreadID_A, NULL, ThreadFunction, (void*) ThreadArgA)) 
+  {
+    fprintf(stderr, "No thread A\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Thread A has ID %lu\n", ThreadID_A);
+
+   // Count Ctrl-Cs pressed; continue upon Enter:
   printf("press ctrl-C for start counting message(1)\n");
   
-
   while(i<25) {
     ;
   }
@@ -33,13 +50,22 @@ int main(void) {
   
   // Restore original SHR:
   sigaction(SIGINT, &oldact, NULL);
-  
   // Wait for Ctrl-C to exit:
   while(1);
   
   return 0;
 }
 
+void *ThreadFunction(void *arg) {
+  printf("%s\n", (char *) arg);  // Cast arg (back) to char*
+  pthread_exit(NULL);            // End of thread function
+  printf("\n");
+  char message[3] = "1\n";
+  for(i = 1; i < 25; ++i)
+  { 
+  write(O_WRONLY, &message, sizeof(message));  // Write message to FIFO
+  sleep(1);
+}}
 // SHR using sa_handler:
 void newHandler()
 {
